6f41b953fcfe7bbd5eabff08321e03594c15bcb9
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-15 17:34:49 +0100

    feat: evento click sulle celle

--- js/main.js
+++ js/main.js
@@ -12,10 +12,16 @@ function myCreateElement(tag, className, content) {
 
 const playBtn = document.querySelector('.btn');
 const board = document.querySelector('.board');
+console.log(board);
 
 playBtn.addEventListener("click", function () {
+    board.innerHTML = '';
     for (let i = 1; i <= 100; i++) {
         const myElement = myCreateElement('div', 'cell', i);
+        myElement.addEventListener("click", function () {
+            myElement.classList.add('active');
+            console.log('Cella', i);
+        })
         board.append(myElement);
     }
 });
