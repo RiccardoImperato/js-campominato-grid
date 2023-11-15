e22517864a02e3b025e41e754c02f16086faee35
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-15 16:40:33 +0100

    feat: click su play genera griglia

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'use strict'
+// Funzioni
+
+function myCreateElement(tag, className, content) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.append(content);
+    return element;
+}
+
+// Programma 
+
+const playBtn = document.querySelector('.btn');
+const board = document.querySelector('.board');
+
+playBtn.addEventListener("click", function () {
+    for (let i = 1; i <= 100; i++) {
+        const myElement = myCreateElement('div', 'cell', i);
+        board.append(myElement);
+        console.log(myElement[i]);
+    }
+});
+
+
+
+
+
